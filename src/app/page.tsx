b1c847a080fcedf0c32fb6d854597fae6eeaa387
Kim O'Dell <kimbo.odell@gmail.com>
2024-09-01 15:09:05 -0400

    Refactor interface data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,52 +8,62 @@ import { format, parseISO } from "date-fns";
 
 
 interface WeatherData {
-  coord: {
-    lon: number;
-    lat: number;
-  };
-  weather: Array<{
-    id: number;
-    main: string;
-    description: string;
-    icon: string;
+  cod: string;
+  message: number;
+  cnt: number;
+  list: Array<{
+    dt: number;
+    main: {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
+      pressure: number;
+      sea_level: number;
+      grnd_level: number;
+      humidity: number;
+      temp_kf: number;
+    };
+    weather: Array<{
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }>;
+    clouds: {
+      all: number;
+    };
+    wind: {
+      speed: number;
+      deg: number;
+      gust: number;
+    };
+    visibility: number;
+    pop: number;
+    sys: {
+      pod: string;
+    };
+    dt_txt: string;
   }>;
-  base: string;
-  main: {
-    temp: number;
-    feels_like: number;
-    temp_min: number;
-    temp_max: number;
-    pressure: number;
-    humidity: number;
-    sea_level: number;
-    grnd_level: number;
-  };
-  visibility: number;
-  wind: {
-    speed: number;
-    deg: number;
-  };
-  clouds: {
-    all: number;
-  };
-  dt: number;
-  sys: {
-    type: number;
+  city: {
     id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
     country: string;
+    population: number;
+    timezone: number;
     sunrise: number;
     sunset: number;
   };
-  timezone: number;
-  id: number;
-  name: string;
-  cod: number;
 }
 
 
+
 export default function Home() {
-  
+
   const { isLoading, error, data } = useQuery<WeatherData>(
     'weatherData',
     async () => {
@@ -72,7 +82,7 @@ export default function Home() {
   if (isLoading) return (
     <div className="flex items-center min-h-screen justify-center">
       <p className="animate-bounce">
-        Loading...      
+        Loading...
       </p>
     </div>
   );
@@ -81,14 +91,14 @@ export default function Home() {
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
       <main className="px-3 max-w-7xl mx-auto flax flex-col gap-9 w-full pb-10 pt-4">
-       <section>
-        <div>
-          <h2 className="flex gap-1 text-2xl text 2xl items-end">
-            {/*<p>{format(parseISO(firstData?.dt_txt ?? ""), "EEEE")}</p>*/}
-          </h2>
-          <div></div>
-        </div>
-       </section>
+        <section>
+          <div>
+            <h2 className="flex gap-1 text-2xl text 2xl items-end">
+              {/*<p>{format(parseISO(firstData?.dt_txt ?? ""), "EEEE")}</p>*/}
+            </h2>
+            <div></div>
+          </div>
+        </section>
       </main>
     </div>
   );
