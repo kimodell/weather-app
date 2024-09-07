440751fb1b2004ed8be58b8742604ab69962de01
Kim O'Dell <kimbo.odell@gmail.com>
2024-09-07 14:13:11 -0400

    Add addtional data for todays weather

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,14 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 import { useQuery } from "react-query";
 import axios from "axios";
-import { format, parseISO } from "date-fns";
+import { format, fromUnixTime, parseISO } from "date-fns";
 import Container from "./components/Container";
 import { convertKelvinToCelsius } from "./utils/convertKelvinToCelsius";
 import WeatherIcon from "./components/WeatherIcon";
 import { getDayOrNightIcon } from "./utils/getDayOrNightIcon";
+import WeatherDetails from "./components/WeatherDetails";
+import { metersToKilometers } from "./utils/metersToKilometers";
+import { convertWindSpeed } from "./utils/convertWindSpeed";
 
 
 type WeatherData = {
@@ -93,6 +96,7 @@ export default function Home() {
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
       <main className="px-3 max-w-7xl mx-auto flax flex-col gap-9 w-full pb-10 pt-4">
+
         {/* todays data */}
         <section className="space-y-4">
           <div className="space-y-2">
@@ -140,7 +144,7 @@ export default function Home() {
 
                     {/*<WeatherIcon iconName={data.weather[0].icon}/>*/}
                     <WeatherIcon iconName={getDayOrNightIcon(data.weather[0].icon, data.dt_txt)} />
-                    
+
                     <p>
                       {convertKelvinToCelsius(data?.main.temp ?? 0)}°
                     </p>
@@ -149,6 +153,38 @@ export default function Home() {
               </div>
             </Container>
           </div>
+          <div className="flex gap-4">
+            {/* left */}
+            <Container className="w-fit justify-center flex-col px-4 items-center">
+              <p className="capitalize text-center">
+                {todaysData?.weather[0].description}{" "}
+              </p>
+              <WeatherIcon
+                iconName={getDayOrNightIcon(
+                  todaysData?.weather[0].icon ?? "",
+                  todaysData?.dt_txt ?? ""
+                )}
+              />
+            </Container>
+            {/* right */}
+            <Container className="bg-blue-200/80  px-6 gap-4 justify-between overflow-x-auto">
+              <WeatherDetails
+                visibility={metersToKilometers(todaysData?.visibility ?? 10000)}
+                humidity={`${todaysData?.main.humidity}%`}
+                windSpeed={convertWindSpeed(todaysData?.wind.speed ?? 1.64)}
+                airPressure={`${todaysData?.main.pressure} hPa`}
+                sunrise={format(fromUnixTime(data?.city.sunrise ?? 1725714010), "H:mm")}
+                sunset={format(fromUnixTime(data?.city.sunset ?? 1725761311), "H:mm")}
+              />
+            </Container>
+
+          </div>
+        </section>
+
+        {/*7 day forecast data */}
+        <section className="flex w-full fox-col gap-4">
+          <p className="text=2xl">7-day Forecast</p>
+
         </section>
       </main>
     </div>
